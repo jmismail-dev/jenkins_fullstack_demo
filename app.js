b926d2c7565d0cb83dab9c9a25b6b216c10d8097
Mohamed Ismail <ismail@valorpaytech.com>
2022-08-16 15:15:33 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const cors = require('cors');
+
+const app = express()
+const port = 5000
+
+
+app.use(cors())
+
+app.get('/api', (req, res) => {
+    res.status(200).send({ status: "OK", message: "API is running successfully!" })
+})
+
+app.listen(port, () => {
+    console.log(`Node App listening on port ${port}`)
+})
---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [state, setState] = useState(0);
+  useEffect(() => {
+    axios.get('/api').then((res) => {
+      setState(res.data)
+    });
+  }, [])
+
+  return (
+    <div className='App'>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      {JSON.stringify(state)}
+    </div>
+  )
+}
+
+export default App
