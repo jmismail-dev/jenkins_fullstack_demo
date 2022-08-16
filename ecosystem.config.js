f3a27ef1cf70b540b0e91ba2596b805d4d355cb6
Mohamed Ismail <ismail@valorpaytech.com>
2022-08-16 16:00:17 +0530

    Changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      script: './app.js',
-      watch: ['/'],
+      script: 'app.js',
+      watch: ['app.js'],
       ignore_watch: ["node_modules", "client"]
     }]
 };
